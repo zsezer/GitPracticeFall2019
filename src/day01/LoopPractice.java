b2f8109aac14cc1132373ad5fa688a327a02662b
zelihasezeer@gmail.com <zelihasezer@gmail.com>
2020-03-13 14:26:25 -0500

    modified

--- src/day01/LoopPractice.java
+++ src/day01/LoopPractice.java
@@ -1,4 +1,11 @@
 package day01;
 
 public class LoopPractice {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i+=2) {
+            System.out.println("Counter= " + i);
+
+
+        }
+    }
 }
